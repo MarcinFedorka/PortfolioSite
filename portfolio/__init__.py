a8f4f7559ddc5ecea33b54da4150902f7a2eaa75
DogSzarik <marcin.fedorowicz1@gmail.com>
2025-08-10 20:51:27 +0100

    add about page

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -3,9 +3,37 @@ from flask import render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Portfolio app with Flask",
+        "thumb": "img/portfolio.jpg",
+        "hero": "img/portfolio.jpg",
+        "categories": ["python", "web", "flask"],
+        "slug": "portfolio",
+        "prod": "https://google.com"
+    },
+    {
+        "name": "Portfolio app with Python Flask",
+        "thumb": "img/portfolio.jpg",
+        "hero": "img/portfolio.jpg",
+        "categories": ["python", "web", "flask"],
+        "slug": "portfolio",
+        "prod": "https://google.com"
+    },
+    {
+        "name": "Portfolio app with Python Flask",
+        "thumb": "img/portfolio.jpg",
+        "hero": "img/portfolio.jpg",
+        "categories": ["python", "web", "flask"],
+        "slug": "portfolio",
+        "prod": "https://google.com"
+    }
+]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 @app.route("/about")
 def about():
